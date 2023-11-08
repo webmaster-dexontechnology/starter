1e5b7a7f8f667e38daa3b818c51dabdc02dda1da
webmaster-dexontechnology <134662978+webmaster-dexontechnology@users.noreply.github.com>
2023-11-08 23:59:01 +0700

    add files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,25 @@
-// https://nuxt.com/docs/guide/directory-structure/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+    app: {
+      cdnURL: 'https://y7sllzb2hyxilfzl5g26cpzf6a0soluc.lambda-url.ap-northeast-1.on.aws/',
+      head: {
+        title: "Title of your website",
+        htmlAttrs: {
+          lang: "en",
+        },
+        meta: [
+          { charset: "utf-8" },
+          { name: "viewport", content: "width=device-width, initial-scale=1" },
+          { hid: "description", name: "description", content: "" },
+          { name: "format-detection", content: "telephone=no" },
+        ],
+      },
+    },
+  
+    devtools: { enabled: true },
+  
+    nitro: {
+      preset: "aws-lambda",
+      inlineDynamicImports: true,
+      serveStatic: true,
+    },  
+  });
